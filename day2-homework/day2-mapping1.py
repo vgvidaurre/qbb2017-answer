aab95076f687d6470b4eaf7f888d1e77437836d2
Gabby Vidaurre <VelindaV@gmail.com>
2017-08-29 17:03:12 -0400

    Correct Code and comments for part 1 of HW 2

--- day2-homework/day2-mapping1.py
+++ day2-homework/day2-mapping1.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
+# import sys
 import sys
 
 f = sys.stdin
 
+# initiate count for the first 100 entries
 count = 0 
+
+# to look at each line in the file
 for line in f:
+    # ensures only the lines with the actual data are being looked at
     if "DROME" not in line:
         continue
     
     else:
+        # ensures only the lines with the essential data are being looked at
         if "FBgn" not in line:
             continue
         else:
-            
+            # splits each item so that columns can be made 
             row = line.split()
+            # ensures on;y the first 100 entries are inlcuded 
             if count <= 100:
                 count = count + 1
+                # print both fly ID and Uniprot ID are printed in the right order with a tab space between
                 print row[-1], "\t", row[-2]
         
             
