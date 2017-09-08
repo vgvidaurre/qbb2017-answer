c536a721c313f9d1956cebbe8e4bf3952b9cef6c
Gabby Vidaurre <VelindaV@gmail.com>
2017-09-07 20:53:08 -0400

    Code and Output for part 4 of lunch exercise Day 5

--- day5-lunch/linereg.py
+++ day5-lunch/linereg.py
@@ -8,17 +8,23 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 #import matplotlip.pyplot as plt
-
-
-tab = open(sys.argv[1])
 avg = []
-for line in tab:
-    fields = line.rstrip("\r\n").split()
-    avg.append(float(fields[4]))
+df_avg = pd.read_csv(sys.argv[1], sep = '\t', header = None, names = ["t_name", "size", "covered", "sum", "mean0", "mean"])
+df_avg = df_avg.sort_values("t_name")
+avg = df_avg["mean0"].values.tolist()
+print df_avg
+#tab = pc.read_csv(arg)
+#avg = []
+#for line in tab:
+    #fields = line.rstrip("\r\n").split()
+    #avg.append(float(fields[4]))
     
 fpkms = []
 df = pd.read_csv( sys.argv[2], sep= "\t")
+df = df.sort_values("t_name")
 fpkms = df["FPKM"].values.tolist()
+print fpkms
+
 
 
 #print len(fpkms), len
--- day5-lunch/marks.py
+++ day5-lunch/marks.py
@@ -6,26 +6,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv( sys.argv[1], sep= "\t")
-coi = ["chr", "start_promoter", "end_promoter", "t_name"]
+df_p = pd.DataFrame()
+df_n = pd.DataFrame()
 
 
 soi_p = df["strand"] == "+"
-
-for strand in df["strand"][soi_p]:
-    df["start_promoter"] = df["start"] - 500
-    df["end_promoter"] = df["start"] + 500
+#for line in opens; fields = line,strip; if fields
+for strand in df[soi_p]:
+    df_p["chromosome"] = df["chr"][soi_p]
+    df_p["start_promoter"] = df["start"][soi_p] - 500
+    df_p["end_promoter"] = df["start"][soi_p] + 500
+    df_p["t_name"] = df["t_name"][soi_p]
 
 soi_n = df["strand"] == "-"
 
-for strand in df["strand"][soi_n]:
-    df["start_promoter"] = df["end"] - 500
-    df["end_promoter"] = df["end"] + 500
-roineg = df["start_promoter"] 
+for strand in df[soi_n]:
+    df_n["chromosome"] = df["chr"][soi_n]
+    df_n["start_promoter"] = df["end"][soi_n] - 500
+    df_n["end_promoter"] = df["end"][soi_n] + 500
+    df_n["t_name"] = df["t_name"][soi_n]
 
-num = df._get_numeric_data()
+df_final = df_p.append(df_n)
+
+
+num = df_final._get_numeric_data()
 num[num < 0] = 0
 
-df[coi].to_csv(sys.argv[2], sep = "\t", index = False, header = False)
+
+
+df_final.to_csv(sys.argv[2], sep = "\t", index = False, header = False)
 
 
 
